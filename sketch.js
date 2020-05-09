88accb2897ab25bfcd14cc4f2ec4a23748cc22df
Gurpreetsingh18 <61836288+Gurpreetsingh18@users.noreply.github.com>
2020-05-09 14:12:54 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+
+var database;
+var player;
+
+var form, game;
+var player1,player2,player3,player4;
+
+
+function setup(){
+  canvas = createCanvas(displayWidth,displayHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+ 
+    
+  
+}
+
+
+function draw(){
+}
